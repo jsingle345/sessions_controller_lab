28e804323c488941b1c7fdcbac08694e1b8d68fe
jsingle345 <jsingle345@gmail.com>
2020-11-06 16:06:20 -0600

    commit tree 4111c06ee95dbff3bce39744d0d0d114b2e0ad8c

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,33 +1,39 @@
 class SessionsController < ApplicationController
 
 
-    def new 
-    end
+   def create 
 
-    def create
         session[:name] = params[:name]
-        if   !params[:name] || params[:name].empty?
+        if !session[:name] || session[:name].empty?
             redirect_to '/login'
-        else
-        redirect_to '/'
+        else 
+            redirect_to '/'
         end
-    end
+   end
 
-    def destroy
-        session.delete :name 
+   def destroy 
+        session.delete :name
+
         redirect_to '/login'
-    end
+   end 
 end
 
-# def create
-#     return redirect_to(controller: 'sessions',
-#                        action: 'new') if !params[:name] || params[:name].empty?
-#     session[:name] = params[:name]
-#     redirect_to controller: 'application', action: 'hello'
-#   end
 
-#   def destroy
-#     session.delete :name
-#     redirect_to controller: 'application', action: 'hello'
-#   end
-# end
+
+
+# def new 
+    # end
+
+    # def create
+    #     session[:name] = params[:name]
+    #     if   !params[:name] || params[:name].empty?
+    #         redirect_to '/login'
+    #     else
+    #     redirect_to '/'
+    #     end
+    # end
+
+    # def destroy
+    #     session.delete :name 
+    #     redirect_to '/login'
+    # end
